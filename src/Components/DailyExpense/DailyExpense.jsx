0ac42049e18f856282fba4d4f8742346729b6dc4
Anushka Maheshwari <anushkamaheshwari@Anushkas-MacBook-Pro-2.local>
2023-12-27 15:43:24 +0530

    changes till incment in salary correctly

--- src/Components/DailyExpense/DailyExpense.jsx
+++ src/Components/DailyExpense/DailyExpense.jsx
@@ -1,38 +1,53 @@
 import data from '../../Data/SourceOfIncome.json';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import axios, * as others from 'axios';
 export default function DailyExpense()
 {
  
-  useEffect(()=>{
-   axios.get('http://localhost:3000/incomeSources').then(
-    res=>{
-     console.log("res is", res )
-    }
-   ).catch(err=>console.log(" error is ", err));
+  const incomeData = useRef(0);
+  const [isSalaryUpdated, setIsSalaryUpdated] = useState(false);
+  const [isDataFetched, setIsDataFetched] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/incomeSources');
+        console.log('res is', response);
+        incomeData.current = response.data[0].amount;
+        setIsDataFetched(true);
+      } catch (error) {
+        console.log('error is', error);
+      }
+    };
+
+    fetchData();
   }, []);
   
    useEffect(()=>{
     const currentDate = new Date();
     const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
     console.log(" last day ", lastDayOfMonth);
-    //const current= new Date();
-    if (currentDate.getDate() === lastDayOfMonth) {
-  
-      axios.put('http://localhost:3000/incomeSources/1', {
-        "name": "Salary",
-        "amount":5000 ,
-        "frequency": "monthly"
-      }).then(resp => {
-      
-          console.log(resp.data);
-      }).catch(error => {
-      
-          console.log(error);
-      });
+    const current= new Date();
+    const updateSalary = async () => {
+      try {
+        const updatedAmount = incomeData.current + 50000;
+
+        const response = await axios.put('http://localhost:3000/incomeSources/1', {
+          name: 'Salary',
+          amount: updatedAmount,
+          frequency: 'monthly'
+        });
+         setIsSalaryUpdated(true);
+        console.log('Salary updated successfully:', response.data);
+      } catch (error) {
+        console.error('Error updating salary:', error);
+      }
+    };
+
+    if (currentDate.getDate() === current.getDate()&& !isSalaryUpdated && isDataFetched) {
+      updateSalary();
     }
-   })
+   },[incomeData, isDataFetched, isSalaryUpdated])
   
   
 }
