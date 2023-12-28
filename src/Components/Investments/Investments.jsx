a5c5ce030207f7fe7ef8710bd4afaf0641b4d11e
anushka maheshwari <anushkamaheshwari02@gmail.com>
2023-12-28 22:42:21 +0530

    changes till pie chart working

--- src/Components/Investments/Investments.jsx
+++ src/Components/Investments/Investments.jsx
@@ -1,6 +1,72 @@
-import InvestmemtsChart from "./InvestmentsChart"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import InvestmentsChart from './InvestmentsChart';
 
-export default function Investmemts()
-{
-  return <InvestmemtsChart></InvestmemtsChart>
-}
+const Investments = () => {
+  const [data, setdata]=useState(null);
+  // Sample data from React
+  const newEquityRecord = {
+    category: 'equity',
+    year: '2023',
+    month: 'January',
+    amount: 20000,
+    date: '2023-01-15',
+    description: 'New Equity Investment',
+  };
+
+  const deleteEquityRecord = {
+    category: 'equity',
+    year: '2023',
+    month: 'January',
+    investmentKey: 'investment4', // Replace with the actual investment key
+  };
+
+  // useEffect(() => {
+  //   // Add a new equity record
+  //   axios.post('http://localhost:5000/investments/addRecord', newEquityRecord)
+  //     .then(response => {
+  //       console.log('Add Equity Record Success:', response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Add Equity Record Error:', error);
+  //     });
+
+  //   // Delete an equity record
+  //   axios.post('http://localhost:5000/investments/deleteRecord', deleteEquityRecord)
+  //     .then(response => {
+  //       console.log('Delete Equity Record Success:', response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Delete Equity Record Error:', error);
+  //     });
+
+  //   // Fetch complete JSON data
+    // axios.get('http://localhost:5000/investments/getCompleteData')
+    //   .then(response => {
+    //     console.log('Complete JSON Data:', response.data);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching complete data:', error);
+    //   });
+  // }, []);
+  useEffect(()=>{
+    axios.get('http://localhost:5000/investments/getCompleteData')
+      .then(response => {
+        setdata(response.data);
+        console.log('Complete JSON Data:', response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching complete data:', error);
+      });
+
+  },[]);
+
+  return ( 
+    <div>
+     
+      <InvestmentsChart investmentdata={data}></InvestmentsChart>
+    </div>
+  );
+};
+
+export default Investments;
--- src/Components/Investments/InvestmentsChart.jsx
+++ src/Components/Investments/InvestmentsChart.jsx
@@ -8,70 +8,69 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 // ... (import statements)
 
-export default function InvestmentsChart() {
-  const [response, setResponse] = useState(null);
+export default function InvestmentsChart(props) {
+  
   const [pieChartData, setPieChartData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const apiResponse = await axios.get('http://localhost:3002/investments');
-        console.log('Investments result is', apiResponse);
-        setResponse(apiResponse);
-      } catch (error) {
-        console.log('error is', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+const response =props.investmentdata;
+console.log("investment data is ",response);
   useEffect(() => {
     var data_for_pie_chart = [];
+    var category_for_pie_chart=[];
     console.log('response is ', response);
-    if (response?.data) {
-      for (let i in response.data) {
-        for (let j in response.data[i]) {
-          var sum = 0;
-          for (let k in response.data[i][j]) {
-            sum = sum + response.data[i][j][k].amount;
+    if (response) {
+      for (const category in response.investments) {
+       var sum=0;
+        category_for_pie_chart.push(category)
+        for (const year in response.investments[category]) {
+          
+          for (const month in response.investments[category][year]) {
+            
+            for (const investmentId in response.investments[category][year][month]) {
+              const investment = response.investments[category][year][month][investmentId];
+              
+              sum=sum+investment.amount;
+            }
           }
-          data_for_pie_chart.push(sum);
-          console.log(data_for_pie_chart);
         }
+      
+        data_for_pie_chart.push(sum);
       }
-      setPieChartData({
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: data_for_pie_chart,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      });
+      if(response)
+      {
+        setPieChartData({
+          labels: category_for_pie_chart,
+          datasets: [
+            {
+              label: 'amount',
+              data: data_for_pie_chart,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        });
+      }
+    
     }
   }, [response]);
 
   return (
     <>
-      {response && response?.data && pieChartData && <Pie data={pieChartData}></Pie>}
+      {response&& pieChartData && <Pie data={pieChartData}></Pie>}
     </>
   );
 }
